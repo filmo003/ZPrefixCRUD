ff65ad269fc1e3f0b97dd89230c07b1d41573013
filmo003 <collin.gilmore47@gmail.com>
2022-05-27 15:56:50 -0400

    everything deploys, issues getting uis to come up

---
+++ api/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const server = require('http').createServer(app);
+const env = process.env.NODE_ENV || 'development';
+const config = require('../knexfile')[env];
+const knex = require('knex')(config);
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (request, response) => {
+    response.status(200).send('App root route running');
+});
+
+module.exports = server;
